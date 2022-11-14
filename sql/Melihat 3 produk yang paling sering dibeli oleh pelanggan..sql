5db983b27e4352d5806d57082ed844cb1cfefba8
raadittt <114294670+raadittt@users.noreply.github.com>
2022-11-14 21:55:51 +0700

    Update Melihat 3 produk yang paling sering dibeli oleh pelanggan..sql

--- sql/Melihat 3 produk yang paling sering dibeli oleh pelanggan..sql
+++ sql/Melihat 3 produk yang paling sering dibeli oleh pelanggan..sql
@@ -1,4 +1,5 @@
-SELECT id_merk, count(id_merk) as jumlah
-FROM pemesanan
-GROUP BY id_merk
-ORDER BY jumlah DESC LIMIT 3;
+SELECT merk.id_kategori, kategori.nama_kategori, count(merk.id_kategori) as jumlah_barang
+FROM merk INNER JOIN kategori
+ON merk.id_kategori = kategori.id
+GROUP BY id_kategori
+ORDER BY jumlah_barang DESC LIMIT 1;
